550e892874b0cdbbeb84f75ddb675d97deb96c41
thaisar13 <thais.02ribeiro@gmail.com>
2025-06-18 06:58:25 -0300

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,11 +1,5 @@
 # streamlit_app.py
 import streamlit as st
-import pandas as pd
-import joblib
-import os
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
 import kagglehub
 
 # Configuração da página
